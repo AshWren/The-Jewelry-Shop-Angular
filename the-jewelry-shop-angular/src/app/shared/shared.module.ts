67c31f4adaeb19bf09f561342fbf8e57b867f101
Rachel Stuckey <rachelstuckey@yahoo.com>
2017-03-28 18:11:47 -0500

    Set Up Some More

--- the-jewelry-shop-angular/src/app/shared/shared.module.ts
+++ the-jewelry-shop-angular/src/app/shared/shared.module.ts
@@ -5,11 +5,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeroRotatorComponent } from './components/hero-rotator/hero-rotator.component';
 import { ItemRotatorComponent } from './components/item-rotator/item-rotator.component';
 import { ThumbnailsComponent } from './components/thumbnails/thumbnails.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HeaderComponent, FooterComponent, HeroRotatorComponent, ItemRotatorComponent, ThumbnailsComponent]
+  declarations: [HeaderComponent, FooterComponent, HeroRotatorComponent, ItemRotatorComponent, ThumbnailsComponent, SearchBarComponent]
 })
 export class SharedModule { }
