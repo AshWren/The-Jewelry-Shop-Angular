ae01f9977ec8894d63eb35993c24d1f43efb1b23
Rachel Stuckey <rachelstuckey@yahoo.com>
2017-03-29 13:42:33 -0500

    Working on header

--- the-jewelry-shop-angular/src/app/shared/components/header/header.component.ts
+++ the-jewelry-shop-angular/src/app/shared/components/header/header.component.ts
@@ -3,7 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  template: `
+    <header class="container">
+      <a class="logo" routerLink="/"><img src="../../../../assets/images/Logo for Jewelry Shop.png"></a>
+      <nav>
+        <ul>
+          <li><a routerLink="/">Home</a></li>
+          <li><a routerLink="/">Home</a></li>
+          <li><a routerLink="/">Home</a></li>
+          <li><a routerLink="/">Home</a></li>
+        </ul>
+      </nav>
+
+  `
 })
 export class HeaderComponent implements OnInit {
 
